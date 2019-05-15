6599565585b325a5991c5eaa31bd57113ef3dd26
William C Barnes <kit_barnes@yahoo.com>
2019-05-15 15:25:03 -0400

    Reordered main.cpp to compile

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -12,7 +12,7 @@
 #include <ESP8266httpUpdate.h>
 #include <FS.h>
 
-const char* version = "0.5";
+const char* version = "0.6";
 
 #define PIN_BUTTON    14  // local control button - short press: on/off, long press adjusts dimming
 #define PIN_LEVEL     13  // PWM output to LEDs - 0 (always low) for off, 1023 (always high) full on
@@ -20,7 +20,6 @@ const char* version = "0.5";
 #define PIN_RED       0   // Adafruit Huzzah red LED - on during setup and when button is pressed
 #define PIN_BLUE      2   // ESP8266 module blue LED - flashing when configuration needed
 
-WiFiClient client;
 ESP8266WebServer server(80);
 
 String devname = String();    // SmartThings name, configuration SSID, and default hostname
@@ -46,116 +45,13 @@ const char *statusWiFi[] = {
 enum {FACTORY_RESET, NOT_ONLINE, WAITING_HUB, NORMAL, UPDATING, UPDATE_FAILED};
 int devState = FACTORY_RESET;
 IPAddress hubIP;        // valid iff
-int hubPort = 0;        //    devState == NORMAL
+int hubPort = 0;        //    hubPort != 0
 // Dimmer State
 bool ledOn = false;
 int ledLevel = 100;     // 1 to 100 - exposed
 int ledRate = 0;        // dim/brighten rate - 0=immediate
 int targetPWM = 0;      // 0 to 1023 - internal
 bool longPress = false; // button is long-pressed (local dimmer control asserted)
-
-void setup() {	
-  devname = String("wcbDimmer")+String(ESP.getChipId(),16);
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();    // need WiFi disconnected to set host name
-  delay(1000);
-  Serial.begin(115200);
-  Serial.println();
-  Serial.printf("//\n//  WiFi 12volt LED Dimmer - Version %s\n//", version);
-  Serial.println();
-  analogWrite(PIN_LEVEL, 0);   // set output off
-  pinMode(PIN_BLUE, OUTPUT); digitalWrite(PIN_BLUE, LOW);	// blue LED on during format
-  pinMode(PIN_BUTTON, INPUT_PULLUP);
-  if (digitalRead(PIN_BUTTON)==LOW) {
-    Serial.println(F("Button pressed - Factory Reset"));
-    SPIFFS.format();
-  }
-  pinMode(PIN_RED, OUTPUT); digitalWrite(PIN_RED, LOW);	// red LED on during setup
-  digitalWrite(PIN_BLUE, HIGH);	// blue LED off during setup
-  Serial.print(F("Starting File System..."));
-  Serial.println(SPIFFS.begin()? F("OK"): F("Failed!"));
-  error = String("");   // no error
-  Serial.print(F("Opening configuration file for read..."));
-  File f = SPIFFS.open(configFile, "r");
-  Serial.println(f? F("OK"): F("Failed!"));
-  if (f) {
-    ssid = String(f.readStringUntil('\n')); ssid.trim();
-    password = String(f.readStringUntil('\n')); password.trim();
-    host = String(f.readStringUntil('\n')); host.trim();
-    f.close();
-    // no need to check these values - they came from the file we wrote. (hopefully)
-    Serial.printf("ssid (%d char) = %s\npassword = %s\nhost = %s\n",
-        ssid.length(), ssid.c_str(), password.c_str(), host.c_str());
-    WiFi.hostname(host);
-    Serial.println(F("connecting"));
-    if (password == "") WiFi.begin(ssid.c_str());         // not tested! (for success)
-    else WiFi.begin(ssid.c_str(),password.c_str());
-    int status = WiFi.waitForConnectResult();
-    if (status == WL_CONNECTED) {
-      Serial.print(F("Connected - IP address: "));
-      Serial.println(WiFi.localIP());
-      Serial.println(F("Successful initialization"));
-      WiFi.softAPdisconnect(true);
-      devState = WAITING_HUB;
-    } else {
-      error = String(statusWiFi[status]);
-      devState = NOT_ONLINE;
-    }
-  } else {
-    Serial.println(F("Failed to open config file"));
-    error = String(F("Please enter WLAN credentials"));
-    ssid = String();
-    password = String();
-    host = String();
-    devState = FACTORY_RESET;
-  }
-  // configure and start http server
-  server.on("/", HTTP_GET, error.length()? sendConfigurationForm: sendDimmerRoot);
-  server.on("/script.js", HTTP_GET, sendJS);
-  server.on("/card", HTTP_GET, handleCardRequest);
-  server.on("/config", HTTP_GET, handleConfiguration);
-  server.on("/updatefirmware", HTTP_GET, handleUpdateFirmware);
-  server.on("/description.xml", HTTP_GET, [](){
-    WiFiClient c = server.client();
-    Serial.print(F("Description request - Remote:"));
-    Serial.print( c.remoteIP() );
-    Serial.print(F(":"));
-    Serial.print( c.remotePort() );
-    Serial.print(F("  Local:"));
-    Serial.print( c.localIP() );
-    Serial.print(F(":"));
-    Serial.println( c.localPort() );
-    SSDP.schema(c);
-  });
-  server.on("/set", HTTP_GET, handleSet);
-  Serial.println(F("Starting server"));
-  server.begin();
-  if (error.length()) {
-    Serial.println(error);
-    Serial.println(F("Configuration required"));
-    WiFi.disconnect(true);
-    // Create a WiFi access point and provide a web server on it.
-    Serial.println(F("Starting configuration access point"));
-    WiFi.softAP(devname.c_str());
-    WiFi.mode(WIFI_AP);             // suspenders AND a belt?
-    Serial.print(F("configuration server at "));
-    Serial.println( WiFi.softAPIP() );
-  } else {  // no error - ready for SmartThings hub
-    Serial.println(F("Starting SSDP"));
-    SSDP.setSchemaURL("description.xml");
-    SSDP.setHTTPPort(80);
-    SSDP.setDeviceType("urn:schemas-upnp-org:device:DimmableLight:1");
-    SSDP.setName(devname);
-    SSDP.setSerialNumber(WiFi.macAddress());
-//    SSDP.setURL("index.html");
-    SSDP.setModelName("uPnP 12Volt LED Dimmer");
-//    SSDP.setModelURL("index.html");
-    SSDP.setManufacturer("WCBarnes Homebuilt");
-//    SSDP.setManufacturerURL("index.html");
-    SSDP.begin();
-  }
-  Serial.println(F("exiting setup()"));
-}
 
 static const char HTTP_START[] PROGMEM = "<!DOCTYPE HTML><html><head>\n\
   <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\
@@ -355,11 +251,39 @@ void handleCardRequest() {
   server.send(200, "text/html", s);
 }
 
+bool parseHubAddr(const String ha, IPAddress ip, int& port){
+  // Set ip and port from ha - returns true on success
+  const char *s = ha.c_str();
+  int x;
+  for (int i = 0; i <= 4; i++,s++) {
+    x = 0;
+    while (*s>='0'&&*s<='9') x = 10*x + *s++ - '0';
+    if (i<4 && x>255) return false;
+    else ip[i] = (byte) x;
+    if (i<3 && *s!='.') return false;
+    if (i==3 && *s!=':') {
+      if (!*s) {
+        Serial.println(F("  parseHubAddr OK - assumes port 80"));
+        port = 80;
+        return true;
+      } else return false;
+    }
+  }
+  if (*--s || !x || x>65535) return false;
+  port = x;
+  Serial.println(F("  parseHubAddr OK"));
+  return true;
+}
+ 
+void setTargetPWM() { targetPWM = (ledOn && ledLevel>0) ? map(ledLevel,1,100,1,1023) : 0; }
+
 void handleSet() {	// execute command from hub or web interface
   Serial.println(F("in handleSet"));
   IPAddress ip;
   int port;
-  if (parseHubAddr(server.arg("hubAddr"),ip,port)) {
+  if (server.hasArg("hubAddr") && parseHubAddr(server.arg("hubAddr"),ip,port)) {
+    Serial.print(F("  hubAddr="));
+    Serial.println(server.arg("hubAddr"));
     devState = NORMAL;
     hubIP = (server.client()).remoteIP();
     hubPort = port;
@@ -396,83 +320,152 @@ void handleSet() {	// execute command from hub or web interface
   server.send(200, "text/plain", "");
 }
 
-bool parseHubAddr(const String ha, IPAddress ip, int& port){
-  // Set ip and port from ha - returns true on success
-  const char *s = ha.c_str();
-  int x;
-  for (int i = 0; i <= 4; i++,s++) {
-    x = 0;
-    while (*s>='0'&&*s<='9') x = 10*x + *s++ - '0';
-    if (i<4 && x>255) return false;
-    else ip[i] = (byte) x;
-    if (i<3 && *s!='.') return false;
-    if (i==3 && *s!=':') {
-      if (!*s) {
-        Serial.println(F("parseHubAddr OK - assumes port 80"));
-        port = 80;
-        return true;
-      } else return false;
-    }
-  }
-  if (*--s || !x || x>65535) return false;
-  port = x;
-  Serial.println(F("parseHubAddr OK"));
-  return true;
-}
- 
 void updateHub() {
+  WiFiClient c;
   Serial.print(F("in updateHub  hub="));
-  Serial.print(hubIP);
-  Serial.print(F(":"));
-  Serial.println(hubPort);
+  Serial.print(hubIP); Serial.print(F(":")); Serial.println(hubPort);
   Serial.print(F(" ledOn=")); Serial.print(ledOn);
   Serial.print(F(" ledLevel=")); Serial.print(ledLevel);
   Serial.print(F(" ledRate=")); Serial.println(ledRate);
-	if (devState == NORMAL) {
-		client.stop();	// end any previous attempt
-		if (client.connect(hubIP,hubPort)) {
+	if (hubPort != 0) {
+		if (c.connect(hubIP,hubPort)) {
 			// send status
-			client.println(F("POST / HTTP/1.1"));
-			client.print(F("HOST: "));
-			client.print(hubIP);
-			client.print(F(":"));
-			client.println(hubPort);
-			client.print(F("Switch: "));
-			client.println(ledOn?F("on"):F("off"));
-			client.print(F("Level: "));
-			client.println(ledLevel);
-			client.print(F("Rate: "));
-			client.println(ledRate);
-			client.println(F("CONTENT-LENGTH: 0"));
-			client.println();
-			client.println();
-			Serial.println(F("Hub updated"));
-			//wait for, but discard, response
+			c.println(F("NOTIFY / HTTP/1.1"));
+			c.print(F("HOST: ")); c.print(hubIP); c.print(F(":")); c.println(hubPort);
+			c.print(F("Switch: ")); c.println(ledOn?F("on"):F("off"));
+			c.print(F("Level: ")); c.println(ledLevel);
+			c.print(F("Rate: ")); c.println(ledRate);
+			c.println(F("CONTENT-LENGTH: 0"));
+			c.println();
+			c.println();
+			Serial.println(F("  Hub updated - Responds:"));
+			//wait for response
 			unsigned long startMS = millis();
-			while (client.connected()) {
-				client.read();
-				if (millis() > startMS + 5000) {
-					devState = WAITING_HUB;
-					Serial.println(F("no response from Hub"));
-					break;
-				}
-			}
-			delay(1);
-			client.stop();
+      while (c.available() == 0) {
+        if (millis() > startMS + 1000) {
+          devState = WAITING_HUB;
+          Serial.println(F("  no response from Hub"));
+          c.stop();
+          return;
+        }
+        delay(1);
+      }
+      while (c.available()) {
+        char ch = static_cast<char>(c.read());
+        Serial.print(ch);
+      }
+      Serial.println();
+			c.stop();
 		} else {
 			devState = WAITING_HUB;
-			Serial.println(F("updateHub couldn't connect"));
+			Serial.println(F("  failed: couldn't connect"));
 		}
-	} 
+	} else {
+    Serial.print(F(" failed: devStste=")); Serial.println(devState);
+  }
 }
 
-void setTargetPWM() { targetPWM = (ledOn && ledLevel>0) ? map(ledLevel,1,100,1,1023) : 0; }
-
-void loop() {
-  server.handleClient();
-  handleButton();
-  adjustPWM();
-  setStatusLEDs();
+void setup() {	
+  devname = String("wcbDimmer")+String(ESP.getChipId(),16);
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();    // need WiFi disconnected to set host name
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.printf("//\n//  WiFi 12volt LED Dimmer - Version %s\n//", version);
+  Serial.println();
+  analogWrite(PIN_LEVEL, 0);   // set output off
+  pinMode(PIN_BLUE, OUTPUT); digitalWrite(PIN_BLUE, LOW);	// blue LED on during format
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
+  if (digitalRead(PIN_BUTTON)==LOW) {
+    Serial.println(F("Button pressed - Factory Reset"));
+    SPIFFS.format();
+  }
+  pinMode(PIN_RED, OUTPUT); digitalWrite(PIN_RED, LOW);	// red LED on during setup
+  digitalWrite(PIN_BLUE, HIGH);	// blue LED off during setup
+  Serial.print(F("Starting File System..."));
+  Serial.println(SPIFFS.begin()? F("OK"): F("Failed!"));
+  error = String("");   // no error
+  Serial.print(F("Opening configuration file for read..."));
+  File f = SPIFFS.open(configFile, "r");
+  Serial.println(f? F("OK"): F("Failed!"));
+  if (f) {
+    ssid = String(f.readStringUntil('\n')); ssid.trim();
+    password = String(f.readStringUntil('\n')); password.trim();
+    host = String(f.readStringUntil('\n')); host.trim();
+    f.close();
+    // no need to check these values - they came from the file we wrote. (hopefully)
+    Serial.printf("ssid (%d char) = %s\npassword = %s\nhost = %s\n",
+        ssid.length(), ssid.c_str(), password.c_str(), host.c_str());
+    WiFi.hostname(host);
+    Serial.println(F("connecting"));
+    if (password == "") WiFi.begin(ssid.c_str());         // not tested! (for success)
+    else WiFi.begin(ssid.c_str(),password.c_str());
+    int status = WiFi.waitForConnectResult();
+    if (status == WL_CONNECTED) {
+      Serial.print(F("Connected - IP address: "));
+      Serial.println(WiFi.localIP());
+      Serial.println(F("Successful initialization"));
+      WiFi.softAPdisconnect(true);
+      devState = WAITING_HUB;
+    } else {
+      error = String(statusWiFi[status]);
+      devState = NOT_ONLINE;
+    }
+  } else {
+    Serial.println(F("Failed to open config file"));
+    error = String(F("Please enter WLAN credentials"));
+    ssid = String();
+    password = String();
+    host = String();
+    devState = FACTORY_RESET;
+  }
+  // configure and start http server
+  server.on("/", HTTP_GET, error.length()? sendConfigurationForm: sendDimmerRoot);
+  server.on("/script.js", HTTP_GET, sendJS);
+  server.on("/card", HTTP_GET, handleCardRequest);
+  server.on("/config", HTTP_GET, handleConfiguration);
+  server.on("/updatefirmware", HTTP_GET, handleUpdateFirmware);
+  server.on("/description.xml", HTTP_GET, [](){
+    WiFiClient c = server.client();
+    Serial.print(F("Description request - Remote:"));
+    Serial.print( c.remoteIP() );
+    Serial.print(F(":"));
+    Serial.print( c.remotePort() );
+    Serial.print(F("  Local:"));
+    Serial.print( c.localIP() );
+    Serial.print(F(":"));
+    Serial.println( c.localPort() );
+    SSDP.schema(c);
+  });
+  server.on("/set", HTTP_GET, handleSet);
+  Serial.println(F("Starting server"));
+  server.begin();
+  if (error.length()) {
+    Serial.println(error);
+    Serial.println(F("Configuration required"));
+    WiFi.disconnect(true);
+    // Create a WiFi access point and provide a web server on it.
+    Serial.println(F("Starting configuration access point"));
+    WiFi.softAP(devname.c_str());
+    WiFi.mode(WIFI_AP);             // suspenders AND a belt?
+    Serial.print(F("configuration server at "));
+    Serial.println( WiFi.softAPIP() );
+  } else {  // no error - ready for SmartThings hub
+    Serial.println(F("Starting SSDP"));
+    SSDP.setSchemaURL("description.xml");
+    SSDP.setHTTPPort(80);
+    SSDP.setDeviceType("urn:schemas-upnp-org:device:DimmableLight:1");
+    SSDP.setName(devname);
+    SSDP.setSerialNumber(WiFi.macAddress());
+    //SSDP.setURL("index.html");
+    SSDP.setModelName("uPnP 12Volt LED Dimmer");
+    //SSDP.setModelURL("index.html");
+    SSDP.setManufacturer("WCBarnes Homebuilt");
+    //SSDP.setManufacturerURL("index.html");
+    SSDP.begin();
+  }
+  Serial.println(F("exiting setup()"));
 }
 
 void handleButton() {
@@ -585,4 +578,11 @@ void setStatusLEDs() {
 		digitalWrite(PIN_RED, red);
 		digitalWrite(PIN_BLUE, blue);
 	}
+}
+
+void loop() {
+  server.handleClient();
+  handleButton();
+  adjustPWM();
+  setStatusLEDs();
 }
